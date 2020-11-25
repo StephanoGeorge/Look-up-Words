4e7c4e69a8aece9c621c484d7819896ac05b54ae
Stephano George <huohuo5230@gmail.com>
2020-11-25 14:50:24 +0800

    Add missing files

---
+++ README.md
@@ -0,0 +1,27 @@
+# Look up Words by Hot Key (快捷键查词)
+
+# Features
+
+- Platforms: Linux, Windows.
+
+- Reads selected text as the word.
+
+- For Windows, registers global hot key to call looking up words, for Linux, please use system setting.
+
+- Speaks the words.
+
+# Dependencies
+
+[`iciba_word`](https://github.com/StephanoGeorge/iciba-API)
+
+Linux: `xclip`
+
+Windows: `keyboard`, `pyperclip`, `win10toast`
+
+# Getting Started
+
+See `python3 look_up_words.py -h`
+
+# Details
+
+Use iciba.com API
---
+++ look_up_words.py
@@ -0,0 +1,70 @@
+import argparse
+import platform
+import shlex
+from subprocess import run, DEVNULL
+
+from iciba_word import Word
+
+p = platform.system()
+no_such_word_expire_time = 3
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--hot-key', default='windows+c',
+                    help='Hot key to call looking up words, only for Windows, for Linux, please use system setting')
+parser.add_argument('--expire-time', default=20, help='Expire time of the notification in second')
+parser.add_argument('--pronunciation-type', default='am',
+                    help="'am' for American, 'en' for English, 'tts' for Text-to-Speak")
+args = parser.parse_args()
+
+if p == 'Linux':
+    pass
+elif p == 'Windows':
+    import keyboard
+    import pyperclip
+    from win10toast import ToastNotifier
+
+    toaster = ToastNotifier()
+else:
+    print(f'Not supported: {platform}')
+    exit(1)
+
+
+def replace(s: str):
+    replaces = (('，', ','), ('。', '.'), ('、', ','), ('（', '('), ('）', ')'), ('：', ':'), ('；', ';'))
+    for rep in replaces:
+        s = s.replace(*rep)
+    return s
+
+
+def look_up():
+    if p == 'Linux':
+        word_str = run(shlex.split('xclip -selection primary -o'), capture_output=True).stdout.decode()
+    else:
+        keyboard.send('ctrl+c')
+        word_str = pyperclip.paste()
+    # word = re.sub(r'[^a-zA-Z0-9]', '', word)
+    word_str = word_str.strip()
+    word = Word(word_str)
+    word_name = word['word_name'] if word.has_word else f'No such word: {word_str}'
+    pronunciation = word['symbols'][0][f'ph_{args.pronunciation_type}'] if word.has_word else ''
+    means = replace('\n'.join(
+        [f"{part['part']}: {','.join(part['means'])}" for part in word['symbols'][0]['parts']]
+    )) if word.has_word else ''
+    word.pronounce(speak=True)
+    if p == 'Linux':
+        if word.has_word:
+            run(shlex.split(f"notify-send.py --expire-time {args.expire_time * 1000} '{word_name}' "
+                            f"'{pronunciation}\n{means}'"), stdout=DEVNULL)
+        else:
+            run(shlex.split(f"notify-send.py --expire-time {no_such_word_expire_time * 1000} "
+                            f"'{word_name}' ''"), stdout=DEVNULL)
+    else:
+        if word.has_word:
+            toaster.show_toast(word_name, f'{pronunciation}\n{means}', duration=args.expire_time)
+        else:
+            toaster.show_toast(word_name, '', duration=no_such_word_expire_time)
+
+
+if p == 'Windows':
+    keyboard.add_hotkey(args.hot_key, look_up)
+    keyboard.wait()
